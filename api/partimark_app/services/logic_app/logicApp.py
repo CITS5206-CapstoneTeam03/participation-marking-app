5df6e3d40f268a894a0ba752be4453f6a2685ef1
Jerry Phan <crazywin3@gmail.com>
2026-05-09 19:30:19 +0800

    refactor: restructure Azure Logic App webhook handling and improve logging

--- api/partimark_app/services/logic_app/logicApp.py
+++ api/partimark_app/services/logic_app/logicApp.py
@@ -1,21 +1,29 @@
-import json
 from fastapi import APIRouter
 from pydantic import BaseModel
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
 # Sample form URL: https://forms.cloud.microsoft/r/3LLRbGEhyZ
 
 class StudentFormData(BaseModel):
+    student_id: str
     first_name: str
     last_name: str
-    avatar_url: str
+    email: str
+    preferred_name: str
+    image_url: str
 
-#TO DO: Implement logic to update database with the received data
 @router.post("/webhook/forms")
 async def handle_forms_webhook(data: StudentFormData):
     # Process the received data here
     print(data)
+
+    # Azure Log Stream
+    logger.info(f"Student data received: {data.model_dump()}")
 
     # TODO Update database with the received data
 
